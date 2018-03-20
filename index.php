4bb8adc3c82651f36cb71eb5c60e6048c4ca8590
reguv760 <reguv760@gmail.com>
2018-03-20 08:48:19 -0700

    minor style fixes: section + blog

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 						<div class="blog-post">
 							<h2><?php the_title(); ?></h2>
-	            <h3><?php the_date(get_option('date_format')); ?></h3>
+	            <h5><?php the_date(get_option('date_format')); ?></h5>
 					    <?php the_content(); ?>
 
 					   </div>
